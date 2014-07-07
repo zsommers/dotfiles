eab8b5c7b85aea8bbdab2299545c02a2707da930
Zach Sommers <zsommers@gmail.com>
2014-07-07 16:29:16 -0700

    Added git setup

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# zsh config
+# zsh setup
 rm -f ~/.zshrc
 rm -rf ~/.zsh
-ln -s "$(pwd)/zsh/zshrc" "~/.zshrc"
-ln -s "$(pwd)/zsh" "~/.zsh"
+ln -s $(pwd)/zsh/zshrc ~/.zshrc
+ln -s $(pwd)/zsh ~/.zsh
+
+# git setup
+rm -f ~/.gitconfig
+rm -f ~/.gitignore_global
+ln -s $(pwd)/git/gitconfig ~/.gitconfig
+ln -s $(pwd)/git/gitignore_global ~/.gitignore_global
